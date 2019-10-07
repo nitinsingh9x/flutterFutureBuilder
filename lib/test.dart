c4373f228882f7bebf7af71cdd16dafa7659056f
Nitin Singh <nitinsingh9x@gmail.com>
2019-10-07 22:18:30 +0530

    second commit

--- lib/test.dart
+++ lib/test.dart
@@ -3,57 +3,30 @@ import 'package:flutter/services.dart';
 
 import 'modal.dart';
 
-
 class App extends StatefulWidget {
-@override
-_AppState createState() => _AppState();
+  @override
+  _AppState createState() => _AppState();
 }
 
 class _AppState extends State<App> {
-
-@override
-Widget build(BuildContext context) {
-  return FutureBuilder(
-    future:  rootBundle.loadString('assets/movies.json'),
-    builder: (BuildContext context, AsyncSnapshot snap){
-      if(snap.hasData){
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: rootBundle.loadString('assets/movies.json'),
+      builder: (BuildContext context, AsyncSnapshot snap) {
+        if (snap.hasData) {
           var movies = movieListFromJson(snap.data);
           return Scaffold(
-             appBar: AppBar(
-              backgroundColor: Colors.deepPurple,
-              title: Text('Movies') 
-            ),
-            body: Column(
-              children: movies.movies.map((movie) => Text(movie.name)).toList(),
-            )
-          );
-      } else {
-         return LoadingScreen();
-      }
-    },
-  );
-}
-}
-
-class Loader extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 250,
-      height: 250,
-      child: CircularProgressIndicator(),
-    );
-  }
-}
-
-class LoadingScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Loader(),
-      ),
+              appBar: AppBar(
+                  backgroundColor: Colors.deepPurple, title: Text('Movies')),
+              body: Column(
+                children:
+                    movies.movies.map((movie) => Text(movie.name)).toList(),
+              ));
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
     );
   }
 }
